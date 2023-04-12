04c0336ed617d8a8418ea94dcc041f38beb01840
amir <amir.hn0159@gmail.com>
2023-04-12 17:35:23 +0430

    footer created

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,35 @@
+import Logo from "@/assets/Logo.png";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="gap-16 justify-center w-5/6 mx-auto md:flex ">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img  src={Logo} alt="footer-logo" />
+          <p className="my-5">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+            nesciunt minima consequuntur, quo cum sed consequatur soluta magni,
+            unde, non reiciendis nulla quae numquam rerum!
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p>Ullamcorper vivamus</p>
+
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+        <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p>(333)425-6825</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
